734fe74d23075899dd55399107f6295a6a5c11b0
fakhriddin01 <a.fakhriddin@gmail.com>
2023-04-29 08:00:50 +0500

    initialize project folder with nest.js

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core"
+
+const start = async () => {
+    try {
+        const app = await NestFactory.create(AppModule, {cors: true})
+        const PORT = process.env.PORT
+
+    app.listen(PORT, ()=> {
+        console.log(`Server is running on ${PORT}`);
+        
+    });
+    } catch (error) {
+        console.log(error);        
+    }    
+}
+
+start();
